3978c43475e28ed771a8b00e8f2e6cc528af729c
wiktor <wiktor.sedzimir@gmail.com>
2024-04-03 16:00:22 +0200

    Working bullet collision

---
+++ space_invader/game_files/bullet.py
@@ -0,0 +1,13 @@
+import game_files.object as ob
+
+class Bullet(ob.Object):
+    def __init__(self, spaceshipXpos, spaceShipYpos, objectSpeed, screenWidth, screenHeight) -> None:
+        image = ob.pygame.image.load("./images/bullet.png")
+        imageWidth = 10
+        imageHeight = 32
+        self.speedFactor = -3
+        super().__init__(spaceshipXpos + 32 - (imageWidth // 2), spaceShipYpos + (imageHeight // 2), 0, objectSpeed*self.speedFactor, 
+                         image, imageWidth, imageHeight, screenWidth, screenHeight)
+    
+    def update(self):
+        self.y += self.ySpeed
--- space_invader/space_invader.py
+++ space_invader/space_invader.py
@@ -1,58 +1,66 @@
 # import pygame
-import game_files.object as gf
+import game_files.object as ob
 from game_files.player import Player
 from game_files.enemy import Enemy
+from game_files.bullet import Bullet
+from collections import deque
 
 class Game:
     def __init__(self) -> None:
         # pygame initialization
-        gf.pygame.init()
+        ob.pygame.init()
 
         # setting screen
         self.screenWidth = 800
         self.screenHeight = 600
-        self.screen = gf.pygame.display.set_mode((self.screenWidth, self.screenHeight))
+        self.screen = ob.pygame.display.set_mode((self.screenWidth, self.screenHeight))
 
-        # setting title and icon
-        gf.pygame.display.set_caption("Space Invaders")
-        self.icon = gf.pygame.image.load("./images/ufo.png")
-        gf.pygame.display.set_icon(self.icon)
+        # setting title, icon and background
+        ob.pygame.display.set_caption("Space Invaders")
+        self.icon = ob.pygame.image.load("./images/ufo.png")
+        ob.pygame.display.set_icon(self.icon)
+        self.backgroundImage = ob.pygame.image.load("./images/background.png")
+
+        # setting background music
+        self.backgroundMusic = ob.pygame.mixer.music.load("./sounds/background.mp3")
 
         # creating player
         self.player = None
 
         # creating enemy
         self.enemy = None
-        self.background = gf.pygame.image.load("./images/background.png")
+
+        # creating bullets queue
+        self.bullets = deque()
 
     def run(self):
         # game loop
+        ob.pygame.mixer.music.play(-1)
         running = True
         objectsSpeed = 5
         self.player = Player(self.screenWidth, self.screenHeight)
         self.enemy = Enemy(self.screenWidth, self.screenHeight, objectsSpeed)
         while running:
-            for event in gf.pygame.event.get():
+            for event in ob.pygame.event.get():
                 match event.type:
-                    case gf.pygame.QUIT:
+                    case ob.pygame.QUIT:
                         running = False
 
-                    case gf.pygame.KEYDOWN:
+                    case ob.pygame.KEYDOWN:
                         match event.key:
-                            case gf.pygame.K_LEFT:
+                            case ob.pygame.K_a:
                                 self.player.xSpeed = -objectsSpeed
-                            case gf.pygame.K_RIGHT:
+                            case ob.pygame.K_d:
                                 self.player.xSpeed = objectsSpeed
+                            case ob.pygame.K_SPACE:
+                                self.bullets.append(
+                                    Bullet(self.player.x, self.player.y, objectsSpeed, self.screenWidth, self.screenHeight)
+                                    )
                             case _:
                                 pass
-                    case gf.pygame.KEYUP:
-                        if event.key == gf.pygame.K_LEFT or event.key == gf.pygame.K_RIGHT:
+                    case ob.pygame.KEYUP:
+                        if event.key == ob.pygame.K_a or event.key == ob.pygame.K_d:
                             self.player.xSpeed = 0
-                        # match event.key:
-                        #     case gf.pygame.K_LEFT:
-                        #         self.player.xSpeed = 0
-                        #     case gf.pygame.K_RIGHT:
-                        #         self.player.xSpeed = 0
 
                     case _:
                         pass
@@ -61,18 +69,42 @@ class Game:
 
             if self.enemy.update() and self.__check_game_over_conditions():
                 running = False
+            
+            self.__update_bullets()
 
             self.__update_screen()
 
-            gf.pygame.display.update()
+            ob.pygame.display.update()
 
     def __update_screen(self):
-        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.backgroundImage, (0, 0))
         self.screen.blit(self.player.image, (self.player.x, self.player.y))
         self.screen.blit(self.enemy.image, (self.enemy.x, self.enemy.y))
+        for bullet in self.bullets:
+            self.screen.blit(bullet.image, (bullet.x, bullet.y))
 
     def __check_game_over_conditions(self) -> bool:
         return self.enemy.y + self.enemy.imageHeight >= self.player.y
+    
+    def __update_bullets(self):
+        i = 0
+        while i < len(self.bullets):
+            self.bullets[i].update()
+            if self.bullets[i].y <= 0:
+                del self.bullets[i]
+            elif Game.__check_collision(self.bullets[i], self.enemy):
+                del self.bullets[i]
+                self.enemy.y = 0
+            else:
+                i += 1
+
+    @staticmethod
+    def __check_collision(bullet, enemy):
+        if (enemy.y <= bullet.y <= enemy.y + enemy.imageHeight) or (bullet.y <= enemy.y <= bullet.y + bullet.imageHeight):
+            if (enemy.x <= bullet.x <= enemy.x + enemy.imageWidth) or (bullet.x <= enemy.x <= bullet.x + bullet.imageWidth):
+                return True
+
+        return False
 
     
 game = Game()
